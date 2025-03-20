be23e5366a9cb35da5c6a1dd8727cccacab7b559
dhruvikparmar <dhruvikparmar1008@gmail.com>
2025-03-20 11:43:53 +0530

    Project2

--- 1.js
+++ 1.js
@@ -8,63 +8,127 @@
 // let p=prompt("Enter Your Name :")
 // let  b=prompt("Enter B Number:")
 // console.log((a!=10)?"true":"false");
-let  a=prompt("Enter \n1 For Pizza\n2 For Burger\n3 Coldrinks")
-if(a==1)
-{
-    document.write("Welcome To Lappinoz Pizza 🍕")
-    let b=prompt("Enter \n1 For Margrita Pizza\n2 For Italien Pizza:")
-    if(b==1)
-    {
-        console.log("You have ordered Margrita Pizza 🍕")
-    }
-    else if(b==2)
-    {
-        console.log("You have ordered Italien Pizza 🍕")
-    }
-    else
-    {
-        console.log("Sorry!! wrong Choice")
-    }
-}
-else if(a==2)
-{
-    document.write("Welcome To Burger King 🍔")
-    let c=prompt("Enter\n1 For Veg. Burger\n2 For Custamized Burger:")
-    if(c==1)
-    {
-        console.log("You have ordered Veg. Burger 🍔")
-    }
-    else if(c==2)
-    {
-        console.log("You have ordered Custamized Burger 🍔")
-    }
-    else
-    {
-        console.log("Sorry!! wrong Choice")
-    }
-}
-else if(a==3)
-{
-    document.write("Welcome Himalaya Coldrinks 🥤")
-    let d=prompt("Enter\n1 For Cola\n2 For Maja\n3 For Sprite")
-    if(d==1)
-    {
-        console.log("You Have Ordered Cola 🥤")
-    }
-    else if(d==2)
-    {
-        console.log("You Have Ordered Maja 🥤")
-    }    
-    else if(d==3)
-    {
-        console.log("You Have Ordered Sprite 🥤")
-    }
-    else
-    {
-        console.log("Sorry!! wrong Choice")
-    }
-}
-else    
-{
-    console.log("Sorry!! wrong Choice");
-}
+// let  a=prompt("Enter \n1 For Pizza\n2 For Burger\n3 Coldrinks")
+// if(a==1)
+// {
+//     document.write("Welcome To Lappinoz Pizza 🍕")
+//     let b=prompt("Enter \n1 For Margrita Pizza\n2 For Italien Pizza:")
+//     if(b==1)
+//     {
+//         console.log("You have ordered Margrita Pizza 🍕")
+//     }
+//     else if(b==2)
+//     {
+//         console.log("You have ordered Italien Pizza 🍕")
+//     }
+//     else
+//     {
+//         console.log("Sorry!! wrong Choice")
+//     }
+// }
+// else if(a==2)
+// {
+//     document.write("Welcome To Burger King 🍔")
+//     let c=prompt("Enter\n1 For Veg. Burger\n2 For Custamized Burger:")
+//     if(c==1)
+//     {
+//         console.log("You have ordered Veg. Burger 🍔")
+//     }
+//     else if(c==2)
+//     {
+//         console.log("You have ordered Custamized Burger 🍔")
+//     }
+//     else
+//     {
+//         console.log("Sorry!! wrong Choice")
+//     }
+// }
+// else if(a==3)
+// {
+//     document.write("Welcome Himalaya Coldrinks 🥤")
+//     let d=prompt("Enter\n1 For Cola\n2 For Maja\n3 For Sprite")
+//     if(d==1)
+//     {
+//         console.log("You Have Ordered Cola 🥤")
+//     }
+//     else if(d==2)
+//     {
+//         console.log("You Have Ordered Maja 🥤")
+//     }    
+//     else if(d==3)
+//     {
+//         console.log("You Have Ordered Sprite 🥤")
+//     }
+//     else
+//     {
+//         console.log("Sorry!! wrong Choice")
+//     }
+// }
+// else    
+// {
+//     console.log("Sorry!! wrong Choice");
+// }
+
+// if(a<b)
+// {
+//     if(a<c)
+//         console.log("a is minimum")
+//     else
+//         console.log("c is minimum")
+// }
+// else if(b<c)
+// {
+//     console.log("b is minimum")
+// }
+// else
+// {
+//     console.log("c is minimum")
+// }   
+// let a=10,b=20
+// let c
+// console.log("a is ",a)
+// console.log("b is ",b)
+// a=a+b
+// b=a-b
+// a=a-b   
+// console.log("After Swapping Value")
+// console.log("a is ",a)
+// console.log("b is ",b)
+// let ch=prompt("Enter Character:")
+// switch(ch)
+// {
+//     case "a":
+//         alert(ch+" is vowel")
+//         break
+//     case "e":
+//         alert(ch+" is vowel")
+//         break
+//     case "i":
+//         alert(ch+" is vowel")
+//         break
+//     case "o":
+//         alert(ch+" is vowel")
+//         break
+//     case "u":
+//         alert(ch+" is vowel")
+//         break
+//     default:
+//         alert(ch+" is not vowel")
+//         break
+// }
+// for(let i=1;i<=10;i++)
+// {
+//     // let c=a*i;
+//     console.log(a,i,a*i);
+// }
+// let a =Number(prompt("Enter A Number for Print Table of That Table :"))
+// console.log(typeof(a))
+// let i=1
+// while(i<=10)
+// {
+//     console.log(a,i,a*i);
+//     i++
+// }
+let btn=document.getElementById("btn");
+let para=document.getElementsByClassName("para")
+console.log(para);
---
+++ p2.js
@@ -0,0 +1,48 @@
+// Q-1 find factor for n number
+// let a=parseInt(prompt("Enter Number :"))
+// for(let i=1;i<=a;i++)
+// {
+//     if(a%i==0)
+//     {
+//         let r=`factor of number ${i}`
+//         console.log(r);
+//     }
+// }
+
+
+// Q-2 find factorial of n number
+// let b=prompt("Enter Number :")
+// let f=1
+// for(let i=1;i<=b;i++)
+// {
+//     f=f*i 
+// }
+// console.log(f);
+
+// Q-3 find sum of all digit     
+// let number = prompt("Enter Number:");
+// let Str = number.toString();
+// let sum=0
+// for(let i=0;i<Str.length;i++)
+// {
+//     sum += parseInt(Str[i]);
+// }
+// document.write("Sum of All digit: " + sum);
+// Q-4 find sum of first an last digit     
+// let number = prompt("Enter Number:");
+
+// let Str = number.toString();
+// let firstDigit = parseInt(Str[0]);
+// let lastDigit = parseInt([Str.length - 1]);
+// let sum = firstDigit + lastDigit;
+// document.write("Sum of first and last digit: " + sum);
+
+// Q-5 print table like 2*1=2
+// let a =Number(prompt("Enter A Number for Print Table of That Table :"))
+// console.log(typeof(a))
+// let i=1
+// while(i<=10)
+// {
+//     console.log(a,i,a*i);
+//     i++
+// }
